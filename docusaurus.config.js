ab44dfea6232bb270a6478f130f1de795b7e3083
Tim Sneath <tim@sneath.org>
2023-05-08 19:21:48 -0700

    Add button for easy link to docs

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -39,18 +39,9 @@ const config = {
       ({
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
           editUrl:
             'https://github.com/dart-windows/website/tree/main/',
         },
-        // blog: {
-        //   showReadingTime: true,
-        //   // Please change this to your repo.
-        //   // Remove this to remove the "edit this page" links.
-        //   editUrl:
-        //     'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
-        // },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
         },
--- src/pages/index.js
+++ src/pages/index.js
@@ -14,13 +14,13 @@ function HomepageHeader() {
       <div className="container">
         <img src="img/dart-on-windows-card.png" width="50%"></img>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
-        {/* <div className={styles.buttons}>
+        {<div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
             to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
+            Read documentation
           </Link>
-        </div> */}
+        </div>}
       </div>
     </header>
   );

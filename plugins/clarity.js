bc28ec675683ba1455fa6faa28719b87bfedaab3
Halil Durmus <durmushalil@proton.me>
2024-05-01 17:21:28 +0300

    Update js code

--- plugins/clarity.js
+++ plugins/clarity.js
@@ -17,11 +17,11 @@ async function microsoftClarity() {
                     {
                         tagName: "script",
                         innerHTML: `
-                      (function(c,l,a,r,i,t,y){
-                          c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-                          t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i+"?ref=gtm2";
-                          y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-                       })(window, document, "clarity", "script", "m4pnkh6qft");`,
+                        (function(c,l,a,r,i,t,y){
+                            c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+                        })(window, document, "clarity", "script", "m4pnkh6qft");`,
                     },
                 ],
             };
